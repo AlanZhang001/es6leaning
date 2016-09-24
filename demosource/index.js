73d36fea7b035048b86c47b48459bb9bc54eaa9f
alanzhang(张伦) <alanzhang@futunn.com>
2016-09-24 20:28:09 +0800

    增加 let const 块级作用域概念

--- demosource/index.js
+++ demosource/index.js
@@ -20,3 +20,15 @@ for (let i = 0; i < 10; i++) {
 }
 
 a[6]();// 6
+
+// ES6的浏览器环境
+function f() { console.log('I am outside!'); }
+(function () {
+  if (false) {
+    // 重复声明一次函数f
+    function f() { console.log('I am inside!'); }
+  }
+
+  f();
+}());
+// Uncaught TypeError: f is not a function
